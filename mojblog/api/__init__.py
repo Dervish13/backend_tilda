e16b484c1bc15a35c290e1fa56859892a9d08487
Rajko Dervisevic <dervish13@tilda.center>
2020-03-10 19:19:23 +0100

    last added schema

--- mojblog/api/__init__.py
+++ mojblog/api/__init__.py
@@ -2,4 +2,11 @@ from freenit.api import register_endpoints
 
 
 def create_api(app):
-    register_endpoints(app, '/api/v0', [])
+    from .blog import blueprint as blog
+    register_endpoints(
+        app,
+        '/api/v0',
+        [
+            blog,
+        ]
+    )
---
+++ mojblog/api/blog.py
@@ -0,0 +1,14 @@
+from freenit.api.methodviews import MethodView
+from flask_smorest import Blueprint
+
+from ..schemas.blog import BlogSchema
+
+blueprint = Blueprint('blog', 'blogs')
+
+
+@blueprint.route('', endpoint='blog')
+
+class BlogListAPI(MethodView):
+    def post(self, args):
+        """Create blog post"""
+        return {}
