e6174b919622583b6dc4fbfea9d8eb28d80d6357
Rajko Dervisevic <dervish13@tilda.center>
2020-04-22 14:47:23 +0200

    21.04 orm, migration, models tilda-backend

--- mojblog/api/blog.py
+++ mojblog/api/blog.py
@@ -1,15 +1,27 @@
 from freenit.api.methodviews import MethodView
 from flask_smorest import Blueprint
+from flask_jwt_extended import get_jwt_identity, jwt_optional, jwt_required
 
 from ..schemas.blog import BlogSchema
+from ..models.blog import Blog
+from ..models.user import User
 
 blueprint = Blueprint('blogs', 'blogs')
 
 
 @blueprint.route('', endpoint='blog')
 class BlogListAPI(MethodView):
+    @jwt_required
     @blueprint.response(BlogSchema)
     @blueprint.arguments(BlogSchema)
     def post(self, args):
         """Create blog post"""
-        return args
+        blog = Blog(**args)
+        user_id = get_jwt_identity()
+        try:
+            user = User.get(id=user_id)
+        except User.DoesNotExist:
+            abort(404, message='User not found')
+        blog.author = user
+        blog.save()
+        return blog
